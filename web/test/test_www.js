9889e6dc64fee2fdceea546e9b42860b63fd7c6f
Aleksey Kabanov <kabkasik@gmail.com>
2018-09-24 08:12:59 +0700

    upgrade test for category post

--- web/test/test_www.js
+++ web/test/test_www.js
@@ -1,32 +1,48 @@
 var request = require('supertest')
     , app = require('../app')
 
-describe("Test http request", function () {
-    it("Show all categories", function (done) {
+describe("Show all categories", function () {
+    it("list categories", function (done) {
         request(app)
             .get("/categories")
             .expect(200, done)
     })
-    it('Create a new category', function (done) {
+});
+describe("Create a new category", function () {
+    it('correct json', function (done) {
         request(app)
             .post('/categories')
             .type('json')
-            .send({'category': { name: "Candy" }})
+            .send({'category': {name: "Candy"}})
             .expect(201, done)
     });
-    it('Show products list from category', function (done) {
+    it('bad json', function (done) {
+        request(app)
+            .post('/categories')
+            .type('json')
+            .send({})
+            .expect(400, done)
+
+    });
+});
+describe("Show products list from category", function () {
+    it('get', function (done) {
         request(app)
             .get("/categories/1/products")
             .expect(200, done)
     });
-    it('Create a new product in the category', function (done) {
+});
+describe("Create a new product in the category", function () {
+    it('correct json', function (done) {
         request(app)
             .post('/categories/1/products')
             .type('json')
             .send({'product': { name: "Butter" , price: 2.6}})
             .expect(201, done)
     });
-    it('Delete product from category', function (done) {
+});
+describe("Delete product from category", function () {
+    it('correct id', function (done) {
         request(app)
             .delete('/products/1')
             .expect(204, done)
